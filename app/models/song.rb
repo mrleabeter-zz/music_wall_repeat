b0115d9499a41095ffe2cd0a6e455ff73e02283d
Christopher Leabeter <christopherleabeter@gmail.com>
2015-10-13 15:08:23 -0400

    Implemented validations for user submit songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
 
+  validates :title, presence: true, length: {in: 5..75}
+  validates :author, presence: true, length: {in: 3..50}
+  validates :url, presence: true
+
 end
